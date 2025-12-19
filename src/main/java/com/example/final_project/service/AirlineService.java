0f4207fa5312cb94fdf2366383f9da17fcadb129
User <daryn060817@gmail.com>
2025-12-19 16:34:30 +0500

    added AirlineService interface

--- src/main/java/com/example/final_project/service/AirlineService.java
+++ src/main/java/com/example/final_project/service/AirlineService.java
@@ -1,4 +1,12 @@
 package com.example.final_project.service;
 
+import com.example.final_project.dto.AirlineDto;
+import java.util.List;
+
 public interface AirlineService {
+    List<AirlineDto> getAll();
+    AirlineDto getById(Long id);
+    AirlineDto addAirline(AirlineDto airline);
+    AirlineDto updateById(Long id, AirlineDto airlineDto);
+    boolean deleteById(Long id);
 }
