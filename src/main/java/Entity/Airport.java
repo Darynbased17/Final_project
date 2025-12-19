a7fa26e48084485d700c7134e57b601bed687416
User <daryn060817@gmail.com>
2025-12-19 14:08:51 +0500

    feat:created Airport entity

--- src/main/java/Entity/Airport.java
+++ src/main/java/Entity/Airport.java
@@ -1,4 +1,27 @@
 package Entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "airports")
 public class Airport {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String code;
+
+    private String name;
+
+    private String city;
+
+    private String country;
 }
