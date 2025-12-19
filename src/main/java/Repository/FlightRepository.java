7749a9c41718cddb9cf15c0903c2ab481845e398
User <daryn060817@gmail.com>
2025-12-19 15:34:10 +0500

    feat: created FlightRepository

---
+++ src/main/java/Repository/FlightRepository.java
@@ -0,0 +1,11 @@
+package Reopsitory;
+
+import Entity.Flight;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface FlightRepository extends JpaRepository<Flight, Long> {
+
+}
