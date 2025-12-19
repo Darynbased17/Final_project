1295a1c5a5d3599064b680392ace70e495a18610
User <daryn060817@gmail.com>
2025-12-19 15:50:00 +0500

    feat: created FlightDto

---
+++ src/main/java/dto/FlightDto.java
@@ -0,0 +1,19 @@
+package dto;
+
+import lombok.*;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class FlightDto {
+    private Long idDto;
+    private String flightNumberDto;
+    private String statusDto;
+    private String departureAirportCodeDto;
+    private String arrivalAirportCodeDto;
+    private AirlineDto airlineDto;
+    private List<AirportDto> airportsDto;
+}
