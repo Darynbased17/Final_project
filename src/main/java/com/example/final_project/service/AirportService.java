a94f1d5ba75625d82eb564791e259e49dfd8e9e2
User <daryn060817@gmail.com>
2025-12-19 16:31:33 +0500

    added AirportService interface

---
+++ src/main/java/com/example/final_project/service/AirportService.java
@@ -0,0 +1,12 @@
+package com.example.final_project.service;
+
+import com.example.final_project.dto.AirportDto;
+import java.util.List;
+
+public interface AirportService {
+    List<AirportDto> getAll();
+    AirportDto getById(Long id);
+    AirportDto addAirport(AirportDto airport);
+    AirportDto updateById(Long id, AirportDto airportDto);
+    boolean deleteById(Long id);
+}
