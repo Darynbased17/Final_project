fa4e02bf972c890b19c384e3fba2cd56537c1de0
User <daryn060817@gmail.com>
2025-12-19 15:53:31 +0500

    feat: created AirlineDto

--- src/main/java/dto/AirlineDto.java
+++ src/main/java/dto/AirlineDto.java
@@ -1,4 +1,14 @@
 package dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class AirlineDto {
+    private Long idDto;
+    private String nameDto;
+    private String countryDto;
 }
