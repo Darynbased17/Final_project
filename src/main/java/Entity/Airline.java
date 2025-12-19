18fa2a4bf87abaa732198b01808da1db6c48d30c
User <daryn060817@gmail.com>
2025-12-19 14:04:34 +0500

    feat: create Airline entity

--- src/main/java/Entity/Airline.java
+++ src/main/java/Entity/Airline.java
@@ -1,4 +1,22 @@
 package Entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "airlines")
 public class Airline {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "airline_id")
+    private Long id;
+
+    private String name;
+
+    private String country;
 }
