f411eba769c8d6eaa2e289b2752b3ebe6c99596f
User <daryn060817@gmail.com>
2025-12-19 14:03:26 +0500

    feat: create Flight entity

--- src/main/java/Entity/Flight.java
+++ src/main/java/Entity/Flight.java
@@ -1,4 +1,34 @@
 package Entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "t_flights")
 public class Flight {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String flightNumber;
+
+    private String status;
+
+    private String departureAirportCode;
+
+    private String arrivalAirportCode;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "airline_id")
+    private Airline airline;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private List<Airport> airports;
 }
