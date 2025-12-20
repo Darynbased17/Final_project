15d6db1e93be026f6d0eb7544cafdc5ca9745357
User <daryn060817@gmail.com>
2025-12-20 22:09:01 +0500

    Created PermissionDto

---
+++ src/main/java/com/example/final_project/dto/PermissionDto.java
@@ -0,0 +1,14 @@
+package com.example.final_project.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PermissionDto {
+    private Long idDto;
+
+    private String nameDto;
+}
