e7e2804fc935324f73a4589fc6aaa4f39f2ad5db
User <daryn060817@gmail.com>
2025-12-20 22:18:00 +0500

    Created UserRepository

---
+++ src/main/java/com/example/final_project/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.final_project.repository;
+
+import com.example.final_project.entity.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, Long> {
+    UserModel findByEmail(String email);
+}
